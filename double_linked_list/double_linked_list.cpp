2d6010e8abb20c2794e13a396e0efdd38be2b494
VinaRahmaVionita <vinarahmavionita824@gmail.com>
2023-05-26 19:49:47 +0700

    menambahkan double LL untuk membaca data dalam node, jika daftar kosong maka akan muncul pesan lis is empty, jika current node sama dengan null maka akan mencetak pesan Record in ascending order of roll number are: dan jika current node tidak smaa dengan null maka akan mencetak pesan kosong

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -98,6 +98,19 @@ bool DoubleLinkedList::deleteNode(int rollNo) {
 bool DoubleLinkedList::listEmpty() {
 	return (START == NULL);
 }
+void DoubleLinkedList::traverse() {
+	if (listEmpty())
+		cout << "\nList is empty" << endl;
+	else {
+		cout << "\nRecord in ascending order of roll number are: " << endl;
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->next;    //step 7 
+		}
+	}
+}
+
 
 
 
