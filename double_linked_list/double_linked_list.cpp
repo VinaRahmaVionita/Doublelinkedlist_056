229c2f9318c1dcd972eac40c7abc217e83aed98f
VinaRahmaVionita <vinarahmavionita824@gmail.com>
2023-05-26 19:30:08 +0700

    menambahkan data di antara 3 node, ketika current next tidak sama dengan null dan no mhs kurang dari nim maka previous = current dan current = current next (untuk menggeser)

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -51,3 +51,12 @@ void DoubleLinkedList::addNode() {
 		return;
 	}
 
+	/*inserting a node between two nodes in the list*/
+	Node* current = START;  //1.a
+	Node* previous = NULL;  //1.b
+	while (current->next != NULL && current->next->noMhs < nim)  //step 1.c
+	{
+		previous = current;  //1.d
+		current = current->next;  //1.e
+	}
+
