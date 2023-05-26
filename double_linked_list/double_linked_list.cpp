868677d94a7c072e571175b0103243f7de8a3bf2
VinaRahmaVionita <vinarahmavionita824@gmail.com>
2023-05-26 19:53:36 +0700

    menamabahkan double LL, jika daftar kosong maka akan mencetak pesan list is empty, jika current node sama dengan start maka muncul pesan Record in descending order of roll number are:, ketika current node next tidak smaa dengan null maka akan terus bergeser ke node berikutnya

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -110,6 +110,21 @@ void DoubleLinkedList::traverse() {
 		}
 	}
 }
+void DoubleLinkedList::revtraverse() {
+	if (listEmpty()) {
+		cout << "\nlist is empty" << endl;
+	}
+	else {
+		cout << "\nRecord in descending order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
+		while (currentNode != NULL) {
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev;
+		}
+	}
+}
 
 
 
